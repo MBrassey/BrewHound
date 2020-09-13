51bf769f3e0e0405fa70d528da485d0b479ee7ad
Mondrovic <mondrovic1@gmail.com>
2020-09-13 11:54:47 -0700

    added a placeholder card

--- assets/scripts/apiFetch.js
+++ assets/scripts/apiFetch.js
@@ -9,18 +9,30 @@ let map;
 // main body function
 function brewerySearch(city, state) {
     axios.get("https://api.openbrewerydb.org/breweries?per_page=8&by_city=" + city + "&by_state=" + state + "&sort=-name")
-        .then(response => brewData(response))
+        // Adds the response.data to brewData function
+        .then(response => brewData(response.data))
         .catch(error => console.log(error));
 }
 
-// Allows data to be used in HTML
+// Allows data to be used in HTML by using the response from fetch
 function brewData(response) {
-    console.log(response.data);
+    brewCards(response)
+
+    // will turn map function to event listener later
     parsedLat = parseFloat(response.data[0].latitude);
     parsedLon = parseFloat(response.data[0].longitude);
     initMap(parsedLat, parsedLon)
 }
 
+
+// Creates cards based on response from API fetch
+function brewCards(response) {
+    console.log(response.data)
+    /* 
+    Needs to create 8 card elements in the "Current City" box that can be clickable to show a map
+    Cards will have to contain data-* values to match latitude and longitude in order to pipe it into the Gmap 
+    */
+}
 
 // Creates map based on brewData values
 function initMap(latitude, longitude) {
