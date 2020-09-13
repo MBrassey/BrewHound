dc45955aa74e80295a97f0fe4763da74a0c67732
Mondrovic <mondrovic1@gmail.com>
2020-09-13 11:03:01 -0700

    added gmaps and restructured code

--- assets/scripts/apiFetch.js
+++ assets/scripts/apiFetch.js
@@ -2,57 +2,35 @@ var cityNameEl = document.querySelector("#city-name");
 var stateNameEl = document.querySelector("#state-name");
 var inputEL = document.querySelector("#input-group");
 
+// defines map for google API
+let map;
+
+
+// main body function
 function brewerySearch(city, state) {
-    axios({
-        method: "GET",
-        url:
-            "https://api.openbrewerydb.org/breweries?per_page=8&by_city=" +
-            city +
-            "&by_state=" +
-            state +
-            "&sort=-name&",
-        headers: {
-            "content-type": "application/json",
-        },
-    })
-        .then((response) => {
-            console.log(response.data);
-            for (i = 0; i < response.data.length; i++)
-                restaurantSearch(
-                    response.data[i].name,
-                    response.data[i].latitude,
-                    response.data[i].longitude
-                );
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    axios.get("https://api.openbrewerydb.org/breweries?per_page=8&by_city=" + city + "&by_state=" + state + "&sort=-name")
+        .then(response => brewData(response))
+        .catch(error => console.log(error));
 }
 
-function restaurantSearch(term, lat, lon) {
-    axios({
-        method: "GET",
-        url:
-            "https://developers.zomato.com/api/v2.1/search?q=" +
-            term +
-            "&lat=" +
-            lat +
-            "&lon=" +
-            lon,
-        headers: {
-            "user-key": "fbb75db1bb45da2de29201a9a3f9cead",
-            "content-type": "application/json",
-        },
-    })
-        .then((response) => {
-            console.dir(reponse.data);
-            console.log(response.data.restaurants[0].restaurant.name);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+// Allows data to be used in HTML
+function brewData(response) {
+    console.log(response.data);
+    parsedLat = parseFloat(response.data[0].latitude);
+    parsedLon = parseFloat(response.data[0].longitude);
+    initMap(parsedLat, parsedLon)
 }
 
+
+// Creates map based on brewData values
+function initMap(latitude, longitude) {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 18
+    });
+}
+
+// submit handler to start main script body
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var city = cityNameEl.value;
@@ -68,4 +46,6 @@ var formSubmitHandler = function (event) {
     }
 };
 
+
+// event listener to run on submit click
 inputEL.addEventListener("submit", formSubmitHandler);
